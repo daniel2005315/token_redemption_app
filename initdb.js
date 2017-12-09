6efa52f6005f468ca9cf75a647969170c00109b7
Daniel_CK <daniel2005315@gmail.com>
2017-12-10 01:45:15 +0800

    added certain pictures in public folder, hard coded image file into database

--- initdb.js
+++ initdb.js
@@ -30,22 +30,22 @@ function populateData() {
   // each item contains the following attributes
   // id, title, description, image, token_value, quantity,createdTimeStamp, tags
   var items = [
-    _item(0, 'Apple','A yummy food','no image',10, 99,  new Date(2016, 11, 10), [t[0], t[1], t[3], t[4]]),
-    _item(1, 'Orange','A yummy food','no image',10, 99, new Date(2016, 10, 10), [t[3], t[5], t[8], t[4]]),
-    _item(2, 'Strawberry','A yummy food','no image',10, 99, new Date(2016, 1, 12), [t[0], t[9], t[3], t[4]]),
-    _item(3, 'Watermelon','A yummy food','no image',10, 99, new Date(2016, 3, 13), [t[0], t[1], t[6], t[4]]),
-    _item(4, 'Pear','A yummy food','no image',10, 99, new Date(2016, 4, 30), [t[0], t[2], t[8], t[7]]),
-    _item(5, 'Peach','A yummy food','no image',10, 99, new Date(2016, 2, 20), [t[1], t[1], t[3], t[4]]),
-    _item(6, 'Pineapple','A yummy food','no image',10, 99, new Date(2016, 1, 15), [t[2], t[7], t[4], t[4]]),
-    _item(7, 'Grape','A yummy food','no image',10, 99, new Date(2016, 1, 1), [t[0], t[9], t[8], t[3]]),
-    _item(8, 'Banana','A yummy food','no image',10, 99, new Date(2016, 6, 20), [t[0], t[2], t[3], t[1]]),
-    _item(9, 'Mango','A yummy food','no image',10, 99, new Date(2016, 3, 10), [t[7], t[1], t[3], t[2]]),
-    _item(10, 'Plum','A yummy food','no image',10, 99, new Date(2016, 1, 20), [t[0], t[1], t[3], t[3]]),
-    _item(11, 'Lychee','A yummy food','no image',10, 99, new Date(2016, 10, 10), [t[0], t[1], t[7], t[4]]),
-    _item(12, 'Kiwi','A yummy food','no image',10, 99, new Date(2016, 9, 11), [t[9], t[4], t[8], t[2]]),
-    _item(13, 'Fig','A yummy food','no image',10, 99, new Date(2016, 1, 12), [t[2], t[1], t[5], t[6]]),
-    _item(14, 'Green Apple','A yummy food','no image',10, 99, new Date(2015, 11, 10), [t[0], t[2], t[4], t[7]]),
-    _item(15, 'Apricot','A yummy food','no image',10, 99, new Date(2015, 2, 12), [t[8], t[0], t[2], t[3]])
+    _item(0, 'Apple','A yummy food','apple.jpg',10, 99,  new Date(2016, 11, 10), [t[0], t[1], t[3], t[4]]),
+    _item(1, 'Orange','A yummy food','Apple.jpg',10, 99, new Date(2016, 10, 10), [t[3], t[5], t[8], t[4]]),
+    _item(2, 'Strawberry','A yummy food','strawberries.jpg',10, 99, new Date(2016, 1, 12), [t[0], t[9], t[3], t[4]]),
+    _item(3, 'Watermelon','A yummy food','Apple.jpg',10, 99, new Date(2016, 3, 13), [t[0], t[1], t[6], t[4]]),
+    _item(4, 'Pear','A yummy food','Apple.jpg',10, 99, new Date(2016, 4, 30), [t[0], t[2], t[8], t[7]]),
+    _item(5, 'Peach','A yummy food','Apple.jpg',10, 99, new Date(2016, 2, 20), [t[1], t[1], t[3], t[4]]),
+    _item(6, 'Pineapple','A yummy food','Apple.jpg',10, 99, new Date(2016, 1, 15), [t[2], t[7], t[4], t[4]]),
+    _item(7, 'Grape','A yummy food','grape.jpg',10, 99, new Date(2016, 1, 1), [t[0], t[9], t[8], t[3]]),
+    _item(8, 'Banana','A yummy food','Apple.jpg',10, 99, new Date(2016, 6, 20), [t[0], t[2], t[3], t[1]]),
+    _item(9, 'Mango','A yummy food','Apple.jpg',10, 99, new Date(2016, 3, 10), [t[7], t[1], t[3], t[2]]),
+    _item(10, 'Plum','A yummy food','Apple.jpg',10, 99, new Date(2016, 1, 20), [t[0], t[1], t[3], t[3]]),
+    _item(11, 'Lychee','A yummy food','Apple.jpg',10, 99, new Date(2016, 10, 10), [t[0], t[1], t[7], t[4]]),
+    _item(12, 'Kiwi','A yummy food','Apple.jpg',10, 99, new Date(2016, 9, 11), [t[9], t[4], t[8], t[2]]),
+    _item(13, 'Fig','A yummy food','Apple.jpg',10, 99, new Date(2016, 1, 12), [t[2], t[1], t[5], t[6]]),
+    _item(14, 'Green Apple','A yummy food','green_apple.jpg',10, 99, new Date(2015, 11, 10), [t[0], t[2], t[4], t[7]]),
+    _item(15, 'Apricot','A yummy food','Aprico.jpg',10, 99, new Date(2015, 2, 12), [t[8], t[0], t[2], t[3]])
   ];
 
   // 11 users
